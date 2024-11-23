9d1845fc934f41ea6a6792bac0fc32cf77b958d6
MAdzikra <muhammad22237@mail.unpad.ac.id>
2024-11-23 22:14:41 +0700

    progress ui

--- md/app/src/main/java/com/example/tepiapp/ui/catalog/CatalogFragment.kt
+++ md/app/src/main/java/com/example/tepiapp/ui/catalog/CatalogFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.tepiapp.Adapter
@@ -30,10 +29,10 @@ class CatalogFragment : Fragment() {
         _binding = FragmentCatalogBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        catalogViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textHome
+//        catalogViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
