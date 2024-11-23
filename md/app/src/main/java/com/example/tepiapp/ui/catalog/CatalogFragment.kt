2c0fb19076a09ce1bae4e509261a83a04fa22868
MAdzikra <muhammad22237@mail.unpad.ac.id>
2024-11-23 11:40:19 +0700

    add configuration, progress ui

--- md/app/src/main/java/com/example/tepiapp/ui/catalog/CatalogFragment.kt
+++ md/app/src/main/java/com/example/tepiapp/ui/catalog/CatalogFragment.kt
@@ -7,15 +7,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.tepiapp.Adapter
 import com.example.tepiapp.databinding.FragmentCatalogBinding
 
 class CatalogFragment : Fragment() {
 
     private var _binding: FragmentCatalogBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var productAdapter: Adapter
+    private lateinit var catalogViewModel: CatalogViewModel
+//    private var productList: List<ListItem> = listOf()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
